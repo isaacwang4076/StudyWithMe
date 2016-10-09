2065daa1973e1d17beeda89d16aad6a7f064f800
Isaac Wang <isaacwang4076@gmail.com>
2016-10-08 18:57:44 -0700

    Starting html and js files

---
+++ helloWorldScript.js
@@ -0,0 +1,8 @@
+function lit_on_click() {
+    document.getElementById('tagline').innerHTML = "Click that shit one more time I fucking dare you";
+	//window.alert("Hey just wanted to alert you a nice big FUCK YOU");
+	//document.write("Hey just wanted to write you a nice big FUCK YOU");
+	console.log("Hey congratulations you opened the console just to receive this FUCK YOU");
+	
+}
+
