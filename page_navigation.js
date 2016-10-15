e7564566e721cf1b26a1acc8846711e7f9fad58c
Isaac Wang <isaacwang4076@gmail.com>
2016-10-14 18:31:11 -0700

    Added Login page

---
+++ page_navigation.js
@@ -0,0 +1,16 @@
+function enterMainPage() {
+    window.location="/main.html"
+}
+
+function enterLoginPage() {
+    window.location="/login.html"
+}
+
+function enterSignUpPage() {
+    window.location="/sign_up.html"
+}
+
+function enterAboutPage() {
+    window.location="/about.html"
+}
+
