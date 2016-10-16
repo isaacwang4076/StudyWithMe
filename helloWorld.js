42517ff4d7df20653365dbbe1832fb07ac2cb55e
Eric Zhang <zhangeric99@gmail.com>
2016-10-15 22:25:43 -0700

    node js begins

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,7 @@
-var sys = require("sys");
-sys.puts("Hello World");
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8000);
