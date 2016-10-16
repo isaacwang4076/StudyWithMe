30a6c2863919797d9f3f072c848d5b465b546997
Isaac Wang <isaacwang4076@gmail.com>
2016-10-15 22:25:59 -0700

    ayy

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,8 @@
-var sys = require("sys");
-sys.puts("Hello World");
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Hello http');
+});
+server.listen(8000);
+
