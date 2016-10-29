e4e13d1dc39cf1fb05dfd51439e54fe7153a4b15
Isaac Wang <isaacwang4076@gmail.com>
2016-10-28 20:28:12 -0700

    some stuff

--- log_in.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-Welcome <?php echo $_POST["email"]; ?><br>
-Your password is: <?php echo $_POST["password"]; ?>
-</body>
-</html>
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /*jslint devel: true */
 /*global $, jQuery, alert*/
 
-function enterMainPage() {
+function enterWelcomePage() {
     "use strict";
-    window.location = "/main.html";
+    window.location = "/welcome.html";
 }
 
 function enterLoginPage() {
@@ -29,6 +29,6 @@ function logIn() {
     }, {});
     console.log(data['email']);
     //var formData = new FormData(document.querySelector('form'))
-    enterMainPage();
+    enterWelcomePage();
 }
 
